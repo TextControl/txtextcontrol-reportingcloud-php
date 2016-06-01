92e6b36cd28857908f6d3e479db9327d8d669cc3
Jonathan Maron <jonathan.maron@theimagingsource.com>
2016-06-01 12:18:26 +0200

    Documentation fixes

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -7,7 +7,7 @@
  *
  * @link      http://www.reporting.cloud to learn more about ReportingCloud
  * @link      https://github.com/TextControl/ReportingCloud.PHP for the canonical source repository
- * @license   https://github.com/TextControl/ReportingCloud.PHP/LICENSE.md New BSD License
+ * @license   https://github.com/TextControl/ReportingCloud.PHP/blob/master/LICENSE.md New BSD License
  * @copyright © 2016 Text Control GmbH
  */
 namespace TXTextControl\ReportingCloud\Exception;
