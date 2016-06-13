00c77efa055f6b1c25ad391c5c87f3e27e796b32
Jonathan Maron <jonathanmaron@users.noreply.github.com>
2016-06-13 07:37:03 +0200

    Moved shared dataProvider to its own omponent

--- test/Filter/TimestampToDateTimeTest.php
+++ test/Filter/TimestampToDateTimeTest.php
@@ -27,24 +27,13 @@ class TimestampToDateTimeTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider defaultProvider
+     * @dataProvider TxTextControlTest\ReportingCloud\Filter\TestAsset\DefaultProvider::defaultProvider
      */
     public function testValid($timeZone, $dateTimeString, $timestamp)
     {
         date_default_timezone_set($timeZone);
 
         $this->assertEquals($dateTimeString, $this->filter->filter($timestamp));
-    }
-
-    public function defaultProvider()
-    {
-        return [
-            ['UTC'          , '2016-06-02T15:49:57+00:00', 1464882597],
-            ['UTC'          , '1970-01-01T00:00:00+00:00', 0],
-            ['Europe/Berlin', '2016-06-02T15:49:57+00:00', 1464882597],
-            ['Asia/Taipei'  , '2009-04-04T12:02:22+00:00', 1238846542],
-            ['Europe/London', '2008-11-17T04:51:53+00:00', 1226897513],
-        ];
     }
 
     /**
