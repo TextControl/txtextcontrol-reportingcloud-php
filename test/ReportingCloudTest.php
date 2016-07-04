b6088e5329b5d9666cfdf1d2ac5db99756b96d2a
Jonathan Maron <jonathanmaron@users.noreply.github.com>
2016-07-04 16:02:35 +0200

    Updated tests for merge and convert to test all return formats

--- test/ReportingCloudTest.php
+++ test/ReportingCloudTest.php
@@ -3,10 +3,12 @@
 namespace TxTextControlTest\ReportingCloud;
 
 use PHPUnit_Framework_TestCase;
+use PHPUnit_Framework_Constraint_IsType as PHPUnit_IsType;
 
-use TxTextControl\ReportingCloud\Exception\RuntimeException;
 use TxTextControl\ReportingCloud\Exception\InvalidArgumentException;
+use TxTextControl\ReportingCloud\Exception\RuntimeException;
 use TxTextControl\ReportingCloud\ReportingCloud;
+use TxTextControl\ReportingCloud\Validator\ReturnFormat as ReturnFormatValidator;
 
 class ReportingCloudTest extends PHPUnit_Framework_TestCase
 {
@@ -433,6 +435,8 @@ class ReportingCloudTest extends PHPUnit_Framework_TestCase
 
     public function testMergeWithTemplateName()
     {
+        $returnFormats        = $this->getTestReturnFormats();
+
         $mergeData            = $this->getTestTemplateMergeData();
         $mergeSettings        = $this->getTestTemplateMergeSettings();
 
@@ -452,14 +456,18 @@ class ReportingCloudTest extends PHPUnit_Framework_TestCase
 
         unlink($tempTemplateFilename);
 
-        $response = $this->reportingCloud->mergeDocument($mergeData, 'PDF', $tempTemplateName, null, false, $mergeSettings);
+        foreach ($returnFormats as $returnFormat) {
 
-        $this->assertNotNull($response);
-        $this->assertNotFalse($response);
-        $this->assertArrayHasKey(0, $response);
-        foreach ($response as $key => $page) {
-            $this->assertTrue(is_integer($key));
-            $this->assertGreaterThanOrEqual(1024, mb_strlen($page));
+            $response = $this->reportingCloud->mergeDocument($mergeData, $returnFormat, $tempTemplateName, null, false, $mergeSettings);
+
+            $this->assertNotNull($response);
+            $this->assertNotFalse($response);
+            $this->assertArrayHasKey(0, $response);
+
+            foreach ($response as $key => $page) {
+                $this->assertInternalType(PHPUnit_IsType::TYPE_INT, $key);
+                $this->assertGreaterThanOrEqual(1024, mb_strlen($page));
+            }
         }
 
         $response = $this->reportingCloud->deleteTemplate($tempTemplateName);
@@ -469,6 +477,8 @@ class ReportingCloudTest extends PHPUnit_Framework_TestCase
 
     public function testMergeWithTemplateFilename()
     {
+        $returnFormats        = $this->getTestReturnFormats();
+
         $mergeData            = $this->getTestTemplateMergeData();
         $mergeSettings        = $this->getTestTemplateMergeSettings();
 
@@ -476,14 +486,18 @@ class ReportingCloudTest extends PHPUnit_Framework_TestCase
 
         $this->assertFileExists($testTemplateFilename);
 
-        $response = $this->reportingCloud->mergeDocument($mergeData, 'PDF', null, $testTemplateFilename, false, $mergeSettings);
+        foreach ($returnFormats as $returnFormat) {
 
-        $this->assertNotNull($response);
-        $this->assertNotFalse($response);
-        $this->assertArrayHasKey(0, $response);
-        foreach ($response as $key => $page) {
-            $this->assertTrue(is_integer($key));
-            $this->assertGreaterThanOrEqual(1024, mb_strlen($page));
+            $response = $this->reportingCloud->mergeDocument($mergeData, $returnFormat, null, $testTemplateFilename, false, $mergeSettings);
+
+            $this->assertNotNull($response);
+            $this->assertNotFalse($response);
+            $this->assertArrayHasKey(0, $response);
+
+            foreach ($response as $key => $page) {
+                $this->assertInternalType(PHPUnit_IsType::TYPE_INT, $key);
+                $this->assertGreaterThanOrEqual(1024, mb_strlen($page));
+            }
         }
     }
 
@@ -583,60 +597,77 @@ class ReportingCloudTest extends PHPUnit_Framework_TestCase
 
     // -----------------------------------------------------------------------------------------------------------------
 
+    protected function getTestReturnFormats()
+    {
+        $validator = new ReturnFormatValidator();
+
+        $ret = $validator->getHaystack();
+
+        return $ret;
+    }
+
     protected function getTestTemplateMergeData()
     {
-        $mergeData = [
-            0 => [
-                'yourcompany_companyname' => 'Text Control, LLC',
-                'yourcompany_zip' => '28226',
-                'yourcompany_city' => 'Charlotte',
-                'yourcompany_street' => '6926 Shannon Willow Rd, Suite 400',
-                'yourcompany_phone' => '704 544 7445',
-                'yourcompany_fax' => '704-542-0936',
-                'yourcompany_url' => 'www.textcontrol.com',
-                'yourcompany_email' => 'sales@textcontrol.com',
-                'invoice_no' => '778723',
-                'billto_name' => 'Joey Montana',
-                'billto_companyname' => 'Montana, LLC',
-                'billto_customerid' => '123',
-                'billto_zip' => '27878',
-                'billto_city' => 'Charlotte',
-                'billto_street' => '1 Washington Dr',
-                'billto_phone' => '887 267 3356',
-                'payment_due' => '20/1/2016',
-                'payment_terms' => 'NET 30',
-                'salesperson_name' => 'Mark Frontier',
-                'delivery_date' => '20/1/2016',
-                'delivery_method' => 'Ground',
-                'delivery_method_terms' => 'NET 30',
-                'recipient_name' => 'Joey Montana',
-                'recipient_companyname' => 'Montana, LLC',
-                'recipient_zip' => '27878',
-                'recipient_city' => 'Charlotte',
-                'recipient_street' => '1 Washington Dr',
-                'recipient_phone' => '887 267 3356',
+        $ret = [
+            [
+                'yourcompany_companyname'  => 'Text Control, LLC',
+                'yourcompany_zip'          => '28226',
+                'yourcompany_city'         => 'Charlotte',
+                'yourcompany_street'       => '6926 Shannon Willow Rd, Suite 400',
+                'yourcompany_phone'        => '704 544 7445',
+                'yourcompany_fax'          => '704-542-0936',
+                'yourcompany_url'          => 'www.textcontrol.com',
+                'yourcompany_email'        => 'sales@textcontrol.com',
+
+                'invoice_no'               => '778723',
+
+                'billto_name'              => 'Joey Montana',
+                'billto_companyname'       => 'Montana, LLC',
+                'billto_customerid'        => '123',
+                'billto_zip'               => '27878',
+                'billto_city'              => 'Charlotte',
+                'billto_street'            => '1 Washington Dr',
+                'billto_phone'             => '887 267 3356',
+
+                'payment_due'              => '20/1/2016',
+                'payment_terms'            => 'NET 30',
+
+                'salesperson_name'         => 'Mark Frontier',
+
+                'delivery_date'            => '20/1/2016',
+                'delivery_method'          => 'Ground',
+                'delivery_method_terms'    => 'NET 30',
+
+                'recipient_name'           => 'Joey Montana',
+                'recipient_companyname'    => 'Montana, LLC',
+                'recipient_zip'            => '27878',
+                'recipient_city'           => 'Charlotte',
+                'recipient_street'         => '1 Washington Dr',
+                'recipient_phone'          => '887 267 3356',
+
+                'total_discount'           => 532.60,
+                'total_sub'                => 7673.4,
+                'total_tax'                => 537.138,
+                'total'                    => 8210.538,
+
                 'item' => [
-                    0 => [
-                        'qty' => '1',
-                        'item_no' => '1',
+                    [
+                        'qty'              => 1,
+                        'item_no'          => 1,
                         'item_description' => 'Item description 1',
-                        'item_unitprice' => '2663',
-                        'item_discount' => '20',
-                        'item_total' => '2130.40',
+                        'item_unitprice'   => 2663,
+                        'item_discount'    => 20,
+                        'item_total'       => 2130.40,
                     ],
-                    1 => [
-                        'qty' => '1',
-                        'item_no' => '2',
+                    [
+                        'qty'              => 1,
+                        'item_no'          => 2,
                         'item_description' => 'Item description 2',
-                        'item_unitprice' => '5543',
-                        'item_discount' => '0',
-                        'item_total' => '5543',
+                        'item_unitprice'   => 5543,
+                        'item_discount'    => 0,
+                        'item_total'       => 5543,
                     ],
                 ],
-                'total_discount' => '532.60',
-                'total_sub' => '7673.4',
-                'total_tax' => '537.138',
-                'total' => '8210.538',
             ],
         ];
 
@@ -644,16 +675,16 @@ class ReportingCloudTest extends PHPUnit_Framework_TestCase
         // total record sets = 5
 
         for ($i = 0; $i < 4; $i++) {
-            array_push($mergeData, $mergeData[0]);
+            array_push($ret, $ret[0]);
         }
 
-        return $mergeData;
+        return $ret;
     }
 
 
     protected function getTestTemplateMergeSettings()
     {
-        $mergeSettings = [
+        $ret = [
 
             'creation_date'              => time(),
             'last_modification_date'     => time(),
@@ -671,12 +702,13 @@ class ReportingCloudTest extends PHPUnit_Framework_TestCase
             'user_password'              => '123456789',
         ];
 
-        return $mergeSettings;
+        return $ret;
     }
 
     protected function getTestTemplateFilename()
     {
         $ret = sprintf('%s/test_template.tx', realpath(__DIR__ . '/TestAsset'));
+
         return $ret;
     }
 
@@ -684,18 +716,21 @@ class ReportingCloudTest extends PHPUnit_Framework_TestCase
     protected function getTempTemplateFilename()
     {
         $ret = sprintf('%s/test_template_%d.tx', sys_get_temp_dir(), rand(0, PHP_INT_MAX));
+
         return $ret;
     }
 
     protected function getTestDocumentFilename()
     {
         $ret = sprintf('%s/test_document.docx', realpath(__DIR__ . '/TestAsset'));
+
         return $ret;
     }
 
     protected function getTempDocumentFilename()
     {
         $ret = sprintf('%s/test_document_%d.docx', sys_get_temp_dir(), rand(0, PHP_INT_MAX));
+
         return $ret;
     }
 
