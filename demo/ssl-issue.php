0ec4399c94c80964a94634d3c5301cc71a75fb37
Jonathan Maron <jonathanmaron@users.noreply.github.com>
2016-07-15 14:04:01 +0200

    Adding debug code

--- demo/ssl-issue.php
+++ demo/ssl-issue.php
@@ -1,10 +1,12 @@
 #!/usr/bin/env php
 <?php
 
+var_dump(curl_version());
+
 for ($x = 0; $x <= 10; $x++) {
 
-    //$uri = "https://api.reporting.cloud";
-    $uri = "https://labs.textcontrol.com/XXX";
+    $uri = "https://api.reporting.cloud";
+    //$uri = "https://labs.textcontrol.com";
 
     printf("Trying %d", $x);
     echo PHP_EOL;
@@ -12,11 +14,10 @@ for ($x = 0; $x <= 10; $x++) {
     $ch = curl_init($uri);
 
     curl_setopt($ch, CURLOPT_VERBOSE        , true);
-    //curl_setopt($ch, CURLOPT_RETURNTRANSFER , 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER , 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 0);
+    curl_setopt($ch, CURLOPT_TIMEOUT        , 2);
     curl_setopt($ch, CURLOPT_SSLVERSION     , $x);
-
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,0);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 2);
 
     if (curl_exec($ch) === false) {
         var_dump(curl_error($ch));
