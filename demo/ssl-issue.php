c25a0bed7924c2111360d8c40eafb146334cf9ba
Jonathan Maron <jonathanmaron@users.noreply.github.com>
2016-07-15 13:58:58 +0200

    Adding debug code

--- demo/ssl-issue.php
+++ demo/ssl-issue.php
@@ -3,14 +3,20 @@
 
 for ($x = 0; $x <= 10; $x++) {
 
+    //$uri = "https://api.reporting.cloud";
+    $uri = "https://labs.textcontrol.com/XXX";
+
     printf("Trying %d", $x);
     echo PHP_EOL;
 
-    $ch = curl_init("https://api.reporting.cloud");
+    $ch = curl_init($uri);
 
     curl_setopt($ch, CURLOPT_VERBOSE        , true);
     //curl_setopt($ch, CURLOPT_RETURNTRANSFER , 1);
     curl_setopt($ch, CURLOPT_SSLVERSION     , $x);
+
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,0);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 2);
 
     if (curl_exec($ch) === false) {
         var_dump(curl_error($ch));
