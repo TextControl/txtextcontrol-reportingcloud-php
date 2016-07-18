b1d16d7011550a7e1335cfe575d27f02c9cdb259
Jonathan Maron <jonathanmaron@users.noreply.github.com>
2016-07-18 07:14:11 +0200

    Adding more debug code

--- src/AbstractReportingCloud.php
+++ src/AbstractReportingCloud.php
@@ -386,6 +386,9 @@ abstract class AbstractReportingCloud
 
         try {
 
+            $options['curl'][CURLOPT_VERBOSE]        = true;
+            $options['curl'][CURLOPT_SSLVERSION]     = 5;
+
             $ret = $client->request($method, $uri, $options);
 
         } catch (\Exception $exception) {
