efae4d3d175d9c76ae375f9655091c4949fa0e28
Jonathan Maron <jonathan.maron@theimagingsource.com>
2018-12-18 15:58:05 +0100

    Added documentation

--- src/StatusCode/AbstractStatusCode.php
+++ src/StatusCode/AbstractStatusCode.php
@@ -23,16 +23,24 @@ namespace TxTextControl\ReportingCloud\StatusCode;
 abstract class AbstractStatusCode
 {
     /**
+     * The HTTP 200 OK success status response code indicates that the request has succeeded.
+     *
      * @see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200
      */
     public const OK = 200;
 
     /**
+     * The HTTP 201 Created success status response code indicates that the request has succeeded and has led to the
+     * creation of a resource.
+     *
      * @see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/201
      */
     public const CREATED = 201;
 
     /**
+     * The HTTP 204 No Content success status response code indicates that the request has succeeded, but that the
+     * client doesn't need to go away from its current page.
+     *
      * @see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/204
      */
     public const NO_CONTENT = 204;
