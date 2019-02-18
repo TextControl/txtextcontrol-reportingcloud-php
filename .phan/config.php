bc42996a99c906e215f467aab572cd71010bbb20
Jonathan Maron <jonathan.maron@theimagingsource.com>
2019-02-18 15:10:57 +0100

    Updated config

--- .phan/config.php
+++ .phan/config.php
@@ -1,17 +1,31 @@
 <?php
 
 return [
-    'target_php_version'              => null,
-    'directory_list'                  => [
+
+    'directory_list' => [
         'src',
         'test',
         'vendor',
     ],
+
     'exclude_analysis_directory_list' => [
         'vendor',
     ],
-    'suppress_issue_types'            => [
+
+    'progress_bar' => true,
+
+    'strict_method_checking' => true,
+
+    'strict_param_checking' => true,
+
+    'strict_property_checking' => true,
+
+    'strict_return_checking' => true,
+
+    'suppress_issue_types' => [
         'PhanUnreferencedUseNormal',
     ],
+
+    'target_php_version' => null,
 
 ];
