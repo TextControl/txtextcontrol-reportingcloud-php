c9d99c29d6bcef6de72e8f2d5d8637d5eed80109
Jonathan Maron <jonathan.maron@theimagingsource.com>
2019-03-01 07:07:10 +0100

    Improving inline comments of demo scripts

--- demo/manage-api-keys.php
+++ demo/manage-api-keys.php
@@ -6,34 +6,39 @@ include_once __DIR__ . '/bootstrap.php';
 use TxTextControl\ReportingCloud\ReportingCloud;
 use TxTextControl\ReportingCloud\Stdlib\ConsoleUtils;
 
-// ---------------------------------------------------------------------------------------------------------------------
+// Instantiate ReportingCloud, using your API key
 
 $reportingCloud = new ReportingCloud([
     'api_key' => ConsoleUtils::apiKey(),
-    'test'    => true,
 ]);
 
+// Loop through all API keys, deleting each one, except for the one you are currently using
+
 foreach ($reportingCloud->getApiKeys() as $apiKey) {
-    if ($apiKey['key'] === ConsoleUtils::apiKey()) {
-        ConsoleUtils::writeLn('Keeping API key "%s".', $apiKey['key']);
+    if ($apiKey['key'] === $reportingCloud->getApiKey()) {
+        ConsoleUtils::writeLn('Keeping API key "%s" (in use).', $apiKey['key']);
         continue;
     }
     ConsoleUtils::writeLn('Deleting API key "%s".', $apiKey['key']);
     $reportingCloud->deleteApiKey($apiKey['key']);
 }
 
+// Create a new API key
+
 $newApiKey = $reportingCloud->createApiKey();
+
+// Destroy the ReportingCloud instance
 
 unset($reportingCloud);
 
-// ---------------------------------------------------------------------------------------------------------------------
+// Instantiate ReportingCloud again, using the new API key created above
 
 $reportingCloud = new ReportingCloud([
     'api_key' => $newApiKey,
 ]);
 
+// Call 2 endpoints to prove the new API key is working
+
 ConsoleUtils::dump($reportingCloud->getAccountSettings());
 
 ConsoleUtils::dump($reportingCloud->getTemplateList());
-
-// ---------------------------------------------------------------------------------------------------------------------
