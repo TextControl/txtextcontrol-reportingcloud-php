55020fe450b7b61633842a190f8a68c0134356c5
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-06-15 11:45:11 +0200

    Added PHPStan baseline

--- src/ReportingCloud.php
+++ src/ReportingCloud.php
@@ -33,7 +33,7 @@ class ReportingCloud extends AbstractReportingCloud
     /**
      * ReportingCloud constructor
      *
-     * @param array $options
+     * @param array<string, bool|int|string> $options
      */
     public function __construct(array $options = [])
     {
